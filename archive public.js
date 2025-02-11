0cee71e0399fac6f526ae6306d972dbe2142bbcf
randomphotoworldwide <gothyukhei@gmail.com>
2025-02-11 21:58:48 +0100

    Update archive public.js

--- archive public.js
+++ archive public.js
@@ -3,14 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Public Archive</title>
+  <title>Photo Archive</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <h1>Public Archive</h1>
-  <a href="index.html">← Back to Upload Page</a>
   <div id="gallery"></div>
-
   <script src="archive.js"></script>
 </body>
 </html>
